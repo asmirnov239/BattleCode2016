51f20eecf03d7fc05cc2ee90d27efedf88dbaebd
allip158 <allip158@gmail.com>
2016-01-16 23:48:51 -0500

    attackDen() called.

--- team333/Soldier.java
+++ team333/Soldier.java
@@ -5,4 +5,16 @@ public class Soldier extends DefaultRobot{
 	public Soldier(RobotController rc) throws GameActionException {
 		super(rc);
 	}
+		
+	@Override
+	public void executeTurn() throws GameActionException{
+		if(!attackRandomZombie()){
+			attackDen();
+			attackWeakest();
+			followDistressSignal();
+			moveRandomly();
+		}
+		return;	
+	}
+	
 }
